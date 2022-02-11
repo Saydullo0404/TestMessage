e7a5334492539b47026a5274f720652d22fc929d
Saydullo <sadi.abdulazizov.2001@gmail.com>
2022-02-11 09:07:27 +0500

    Initial Commit

---
+++ TestMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMessage
+//
+//  Created by 1 on 11/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
